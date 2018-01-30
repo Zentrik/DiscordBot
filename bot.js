bc20a75ad786f892e4ad766e02485fd37812bbf7
Zentrik <Zentrik@users.noreply.github.com>
2018-01-30 11:36:46 +0000

    Add files via upload
    updated link with new permissions

--- bot.js
+++ bot.js
@@ -234,7 +234,7 @@ bot.on('message', function(message) {
         .addField('Name', 'NeonNukeBot')
         .addField('Purpose', 'Dankness')
         .addField('Maker', 'NeonNuke#1160')
-        .addField('To add me to your server visit:', 'https://discordapp.com/oauth2/authorize?client_id=373462552614797312&scope=bot&permissions=305155072')
+        .addField('To add me to your server visit:', 'https://discordapp.com/oauth2/authorize?client_id=373462552614797312&scope=bot&permissions=303057922')
         .addField('My Github', 'https://github.com/Zentrik/DiscordBot')
         .setThumbnail(bot.user.avatarURL)
       message.channel.send(embed);
